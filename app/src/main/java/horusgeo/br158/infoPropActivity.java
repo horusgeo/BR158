43c034a6188ccf9774cbfa3bc25628d983be110a
HORUS03 <mugrah@gmail.com>
2017-02-24 11:10:13 -0300

    Info Prop Confrontantes not showing

--- app/src/main/java/horusgeo/br158/infoPropActivity.java
+++ app/src/main/java/horusgeo/br158/infoPropActivity.java
@@ -3,11 +3,14 @@ package horusgeo.br158;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class infoPropActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_prop);
+
     }
 }
