c2cd608a3e59f722b7553e54f80c726401fa8d05
HORUS03 <mugrah@gmail.com>
2017-03-08 14:53:27 -0300

    infoprop.xml fixed

--- app/src/main/java/horusgeo/br158/Benfeitorias.java
+++ app/src/main/java/horusgeo/br158/Benfeitorias.java
@@ -17,4 +17,51 @@ public class Benfeitorias {
 
     }
 
+    public Boolean getConstrucoes() {
+        return construcoes;
+    }
+
+    public void setConstrucoes(Boolean construcoes) {
+        this.construcoes = construcoes;
+    }
+
+    public Boolean getEquipamentos() {
+        return equipamentos;
+    }
+
+    public void setEquipamentos(Boolean equipamentos) {
+        this.equipamentos = equipamentos;
+    }
+
+    public Boolean getCroquis() {
+        return croquis;
+    }
+
+    public void setCroquis(Boolean croquis) {
+        this.croquis = croquis;
+    }
+
+    public String getConstucoesText() {
+        return constucoesText;
+    }
+
+    public void setConstucoesText(String constucoesText) {
+        this.constucoesText = constucoesText;
+    }
+
+    public String getEquipamentosText() {
+        return equipamentosText;
+    }
+
+    public void setEquipamentosText(String equipamentosText) {
+        this.equipamentosText = equipamentosText;
+    }
+
+    public String getCroquisText() {
+        return croquisText;
+    }
+
+    public void setCroquisText(String croquisText) {
+        this.croquisText = croquisText;
+    }
 }
